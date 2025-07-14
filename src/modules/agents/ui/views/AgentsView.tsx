516b7adc8f7468ab6af729f3156541a75cadb0d6
Sathyam Sahu <sathyamrock55@gmail.com>
2025-07-14 13:30:53 +0530

    14:Agent-Page-done

--- src/modules/agents/ui/views/AgentsView.tsx
+++ src/modules/agents/ui/views/AgentsView.tsx
@@ -9,11 +9,12 @@ import { columns } from "../components/Columns";
 import EmptyState from "@/components/EmptyState";
 import { useAgentsFilters } from "../../hooks/use-agents-filter";
 import DataPagination from "../components/DataPagination";
+import { useRouter } from "next/navigation";
 
 const AgentsView = () => {
 
     const [filters, setFilters] = useAgentsFilters(); 
-
+    const router = useRouter(); 
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
         ...filters 
@@ -21,7 +22,11 @@ const AgentsView = () => {
 
     return (
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data.items} columns={columns}/>
+            <DataTable 
+                data={data.items} 
+                columns={columns}
+                onRowClick={(row) => router.push(`/agents/${row.id}`)}
+            />
             <DataPagination
                 page={filters.page}
                 totalPages={data.totalPages}
